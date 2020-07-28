07bc48b3742e87734e656f4b24a18012be82c759
jasonmccb <jasonmccb@163.com>
2020-07-28 23:28:18 +1200

    Qt5 main window built by CMake.

---
+++ ForegroundExtractionApp/ForegroundExtractionApp.cpp
@@ -0,0 +1,15 @@
+﻿// ForegroundExtractionApp.cpp : Defines the entry point for the application.
+//
+
+#include "ForegroundExtractionApp.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Hello CMake." << endl;
+	QApplication app(argc, argv);
+	MainWindow mainWindow;
+	mainWindow.show();
+	return app.exec();
+}
---
+++ ForegroundExtractionApp/ForegroundExtractionApp.h
@@ -0,0 +1,10 @@
+﻿// ForegroundExtractionApp.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+#include <QApplication>
+#include "mainwindow.h"
